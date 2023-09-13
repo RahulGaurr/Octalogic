4d7068384c92b112422e5b6821c27cc9f0e52d1f
rahul gaur <rahulgaurr09@gmail.com>
2023-09-13 15:04:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import React from 'react';
+import Home from './Pages/HomePage';
+import Signup from './components/Signup';
+import { Routes, Route} from 'react-router-dom'
+import Signin from './components/Signin';
+
+
+const App = () => {
+  return (
+    <>
+    <Routes>
+            <Route path="/" element={<Signup/>} />
+            <Route path="/signin" element={<Signin/>} />
+            <Route path="/home" element={<Home/>} />
+    </Routes>
+   </>
+  );
+};
+
+export default App;
